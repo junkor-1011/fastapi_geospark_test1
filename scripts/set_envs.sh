09ec294137bfb37b0293391336f551c4f5d88ef9
test-user <test@email.com>
2020-06-28 21:03:10 +0900

    WIP: set local-spark setting

--- scripts/set_envs.sh
+++
@@ -1,5 +0,0 @@
-export PROJECT_HOME=$(cd $(dirname $0)/..; pwd)
-export SPARK_WAREHOUSE_HOME=$PROJECT_HOME/hive-db/spark-warehouse
-export HIVE_METASTORE_DB_HOME=$PROJECT_HOME/hive-db
-
-export PYTHONPATH=$PROJECT_HOME/app/libs:$PYTHONPATH
