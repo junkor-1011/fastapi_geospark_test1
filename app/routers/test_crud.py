e4d7f999660f10adfdb3589b86420d89bfce93e4
test-user <test@email.com>
2020-06-28 23:28:41 +0900

    async

--- app/routers/test_crud.py
+++ app/routers/test_crud.py
@@ -10,8 +10,14 @@ from ..database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
-router = APIRouter()
 
+def create_router():
+    """create router"""
+    _router = APIRouter()
+    return _router
+
+
+router = create_router()
 
 # Dependency
 def get_db():
@@ -23,7 +29,7 @@ def get_db():
 
 
 @router.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = cruds.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
@@ -31,13 +37,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = cruds.get_users(db, skip=skip, limit=limit)
     return users
 
 
 @router.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+async def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = cruds.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -45,13 +51,13 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 
 
 @router.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(
+async def create_item_for_user(
     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
 ):
     return cruds.create_user_item(db=db, item=item, user_id=user_id)
 
 
 @router.get("/items/", response_model=List[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = cruds.get_items(db, skip=skip, limit=limit)
     return items
